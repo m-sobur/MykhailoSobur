30c3cccf88a60f62f5878a0723cc12bf108303a9
Mykhailo_Sobur <929930MBmb>
2022-07-06 19:15:24 +0300

    Removed 1 redundant line break

--- src/main/java/com/epam/spring/homework1/pet/Pet.java
+++ src/main/java/com/epam/spring/homework1/pet/Pet.java
@@ -3,7 +3,6 @@ package com.epam.spring.homework1.pet;
 import com.epam.spring.homework1.interfaces.Animal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
 
 @Component
