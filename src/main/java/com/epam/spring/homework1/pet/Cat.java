859230023f5a20482ed2205c72a6d5658070c3db
Mykhailo_Sobur <929930MBmb>
2022-07-07 12:44:01 +0300

    Added 1 space between 'Animal' and '{'

--- src/main/java/com/epam/spring/homework1/pet/Cat.java
+++ src/main/java/com/epam/spring/homework1/pet/Cat.java
@@ -4,5 +4,5 @@ import com.epam.spring.homework1.interfaces.Animal;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cat implements Animal{
+public class Cat implements Animal {
 }
